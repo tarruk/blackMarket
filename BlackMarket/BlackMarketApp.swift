99719eb5aa6c46a1d779e9250e66e7a8a5cf426b
Tarek Radovan <tarek.radovan@gmail.com>
2022-12-06 11:35:54 -0300

    first commit

---
+++ BlackMarket/BlackMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackMarketApp.swift
+//  BlackMarket
+//
+//  Created by Tarek Radovan on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BlackMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
